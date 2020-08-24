1218774fe3421acee0405b16bad14d6ef56a330a
dachuwu <55524523+dachuwu@users.noreply.github.com>
2020-08-24 13:01:28 +0800

    update meeting

--- TWmap demo.R
+++ TWmap demo.R
@@ -1,41 +1,61 @@
 
+
+
+### https://github.com/dachuwu/TWmap
+### download `TWmap demo.R`
+
+
 ### 0. Installation
+install.packages("devtools")
 devtools::install_github("dachuwu/TWmap")
 require(TWmap)
 require(sf)
 require(dplyr)
 
+
+
 ### 1. Preparation
 View(dict_admin) # dictionary
-View(attri_death_pm25) # demo dataset
+View(attri_death_pm25) # demo data set
 
-
+#
 moh2moi <- structure(dict_admin$town_moi, names = dict_admin$town_moh)
-df <- attri_death_pm25 %>%
-  mutate(town.moi = moh2moi[towncode_new])
+
+df <- attri_death_pm25
+df$town.new <- moh2moi[df$towncode_new]
+
+
+# df <- attri_death_pm25 %>%
+#   mutate(town.moi = moh2moi[towncode_new])
+
 
 
 
 ### 1. county-specific measures (continuous scale)
 df <- df %>%
-  dplyr::mutate(county = substr(town.moi, 1, 5))%>%
+  dplyr::mutate(county = substr(town, 1, 5))%>%
   dplyr::group_by(county)%>%
   dplyr::summarise(xv = sum(all_att_death))%>%
   dplyr::ungroup()
 
-TWmap::twmap_static(geo.code = df$county, x = df$xv,
-                    geo.level = "county", x.name = "all_att_death")
+TWmap::twmap_static(geo.code = df$county, x = df$xv, geo.level = "county",
+                    x.name = "all_att_death",
+                    col.control = list(option = "D", dir = -1, na.color="black"),
+                    thm.control = list(border = "white", back = "grey90")
+                    )
 
 
 
 ### 2. county-specific measures (discrete scale)
-df <- df%>%
+df <- df %>%
   mutate(
-    xv_d = cut(xv, breaks = seq(0, 700, 100))
+    xv_d = cut(xv, breaks = c(seq(0, 500, 100), Inf))
   )
 
 TWmap::twmap_static(geo.code = df$county, x = df$xv_d,
                     geo.level = "county", x.name = "all_att_death")
+
+
 
 
 ### 3. township-specific measures (continuous scale)
@@ -54,12 +74,16 @@ df <- df %>%
                labels = c("0~1000", "1000~2000", "2000~3000","3000~4000", "4000+"))
   )
 
-TWmap::twmap_static(geo.code = df$town, x = df$xv_d,
+G <- TWmap::twmap_static(geo.code = df$town, x = df$xv_d,
                     geo.level = "town", x.name = "YLD rate per 100,000")
 
 
 
-### 4. misc
+### 4. Output
+require(ggplot2)
+G2 <- G +
+  labs(title = "HAHAHA", subtitle= "YLD of liver cancer")+
+  theme(legend.position = "right")
 
-
-
+ggplot2::ggsave(filename = "tmp.jpeg", plot = G2,
+                width = 22, height = 15,units = "cm", dpi=300)
